15d86d62629ab12cdfd22865b470449340c0b6af
Shashikant Giri <Shashikantshankar707@gmail.com>
2025-11-20 17:40:59 +0530

    Update content.jsx

--- src/components/content.jsx
+++ src/components/content.jsx
@@ -26,28 +26,7 @@ const content = () => {
           </div>
         </div>
         <div className='w-full h-[85%] grid grid-cols-4 grid-rows-2 justify-center items-center gap-1.5'>
-          <div className='w-[99%] h-[380px] border-2 border-amber-300 rounded-xl shadow-lg shadow-gray-500 flex flex-col justify-start items-center'>
-            <div className='w-[99%] h-[60%] flex justify-center items-center'>
-              <img src="https://marketplace.canva.com/EAGHWOQ105U/1/0/1131w/canva-white-and-green-simple-business-report-cover-page-a4-document-V10S_ZQxLkU.jpg" alt="" className='w-[99%] h-[99%] rounded-[10px]' />
-            </div>
-            <div className='w-[99%] h-[40%] p-0.5 flex flex-col justify-between items-center'>
-              <div className='w-[99%] h-[49%] flex justify-center items-center'>
-                <h4>Temp pdf files</h4>
-              </div>
-              <div className='w-[99%] h-[49%] flex flex-row justify-center items-center'>
-                <div className='w-[49%] h-[99%] flex justify-center items-center'>
-                  <button className='w-[80%] h-[70%] border-none text-white rounded-[10px] bg-[#2a2ac2] flex justify-center items-center'>
-                    <a href="./Temp_files.pdf" className='text-white'>
-                      Read
-                    </a>
-                  </button>
-                </div>
-                <div className='w-[49%] h-[99%] flex justify-center items-center'>
-                  <a href="./Temp_files.pdf" className='w-[80%] h-[70%] border-none text-white rounded-[10px] bg-[#2a2ac2] flex justify-center items-center' download><button>Download</button></a>
-                </div>
-              </div>
-            </div>
-          </div>
+          
           {api.map((api) => {
             if (api.id <= 8) {
               return (<div className='w-[99%] h-[380px] border-2 border-amber-300 rounded-xl shadow-lg shadow-gray-500 flex flex-col justify-start items-center'>
